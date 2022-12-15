e44b9082be9a04353c1b1d7a5cb3feb75d1dd5e4
Artem <myartoym@gmail.com>
2022-12-15 21:02:08 +0300

    the layout of the table component has been made

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div class="tableContainer">
+      <div class="button">Добавить</div>
+      <theTable />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import theTable from './components/theTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    theTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.tableContainer{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+.button{
+  width: 150px;
+  height: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgb(47, 47, 142);
+  border-radius: 32px;
+  transition: .2s;
+  cursor: pointer;
+}
+.button:hover{
+  background-color: rgb(47, 47, 142);
+  color: #fafafa;
+}
+</style>
